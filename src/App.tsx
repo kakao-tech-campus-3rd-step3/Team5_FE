a87a3ac35e81f45e0d43844cbdf06a42e476a8b8
Changmok LEE <cmlee5075@gmail.com>
2025-09-08 20:44:38 +0900

    refactor: App.tsx 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { ThemeProvider } from '@emotion/react';
-import { useState } from 'react';
 import { theme } from './styles/theme';
-import FeedbackPage from './pages/Feedback'; 
+// import FeedbackPage from './pages/Feedback';
+import ArchivePage from './pages/Archive/Archive';
+import FeedbackPage from './pages/Feedback/Feedback';
+// import SubscribePage from './pages/subscribe';
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <ThemeProvider theme={theme}>
-      {/* <div className="App">
-        <h1>DailyQ 프로젝트</h1>
-        <p>협업을 위한 깔끔한 시작점입니다.</p>
-        <button onClick={() => setCount(count + 1)}>카운트: {count}</button>
-      </div> */}
+      {/* <ArchivePage /> */}
+      {/* <SubscribePage /> */}
       <FeedbackPage />
     </ThemeProvider>
   );
 }
 
 export default App;
-
-
