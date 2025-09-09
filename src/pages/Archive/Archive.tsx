990e77ca31336bbfb4f35971022f84465a50075f
Changmok LEE <cmlee5075@gmail.com>
2025-09-10 00:11:43 +0900

    feat: lottie 에니메이션 적용(WIP)

--- src/pages/Archive/Archive.tsx
+++ src/pages/Archive/Archive.tsx
@@ -3,6 +3,8 @@ import Description from './components/Description';
 import PinnedQuestionList from './components/PinnedQuestionList';
 import QuestionList from './components/QuestionList';
 import useSectionScroll from './hooks/useSectionScroll';
+import Lottie from 'lottie-react';
+import clickkAnimation from './assets/lottie/clickIcon.json';
 
 const ArchivePage = () => {
   const {
@@ -13,7 +15,13 @@ const ArchivePage = () => {
     <Wrapper>
       <SectionFirst ref={sectionFirstRef}>
         <Description />
-        <ScrollDownButton onClick={handleDownClick}>Down</ScrollDownButton>
+        <ButtonWrapper>
+          <ScrollDownButton onClick={handleDownClick}>down</ScrollDownButton>
+        </ButtonWrapper>
+        {/* <LottieWrapper> */}
+        {/* <Lottie animationData={clickkAnimation} loop autoplay /> */}
+        {/* </LottieWrapper> */}
+
         <PinnedQuestionList />
       </SectionFirst>
 
@@ -52,6 +60,7 @@ const SectionFirst = styled.section`
 `;
 
 const SectionSecond = styled.section`
+  position: relative;
   height: 100vh;
   scroll-snap-align: start;
 
@@ -62,6 +71,31 @@ const SectionSecond = styled.section`
   background-color: #ff6d6d;
 `;
 
-const ScrollDownButton = styled.button``;
+const ButtonWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: 10%;
+  transform: translate(-50%, -50%);
+`;
 
-const ScrollUpButton = styled.button``;
+const ScrollDownButton = styled.button`
+  width: 100%;
+  height: 100%;
+`;
+
+const ScrollUpButton = styled.button`
+  position: absolute;
+  left: 50%;
+  bottom: 10%;
+  transform: translate(-50%, -50%);
+`;
+
+// const LottieWrapper = styled.div`
+//   position: absolute;
+//   left: 50%;
+//   bottom: 10%;
+//   transform: translate(-50%, -50%);
+
+//   // width: 80px;
+//   // height: 80px;
+// `;
