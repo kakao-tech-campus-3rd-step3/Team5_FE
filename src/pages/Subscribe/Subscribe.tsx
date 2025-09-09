624dee0e3511cea072990815ff92473164444f20
JinVibe <loonaticvibe2.11@gmail.com>
2025-09-09 19:53:54 +0900

    fix: GNB 컴포넌트 분리

--- src/pages/Subscribe/Subscribe.tsx
+++ src/pages/Subscribe/Subscribe.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import GNB from '../../components/GNB/GNB';
 
 // 통화 포맷팅 유틸리티 함수
 const formatCurrency = (amount: number): string => {
@@ -35,20 +36,7 @@ const SubscribePage: React.FC = () => {
 
       <SubscribeButton>구독하기</SubscribeButton>
 
-      <BottomNavigation>
-        <NavItem>
-          <HomeIcon />
-        </NavItem>
-        <NavItem>
-          <SearchIcon />
-        </NavItem>
-        <NavItem>
-          <DownloadIcon />
-        </NavItem>
-        <NavItem>
-          <ProfileIcon />
-        </NavItem>
-      </BottomNavigation>
+      <GNB />
     </Container>
   );
 };
@@ -169,154 +157,3 @@ const SubscribeButton = styled.button`
   }
 `;
 
-const BottomNavigation = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: #f5f5dc;
-  padding: 12px 0;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  border-top: 1px solid #e0e0e0;
-`;
-
-const NavItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  cursor: pointer;
-  padding: 8px;
-  transition: opacity 0.2s ease;
-
-  &:hover {
-    opacity: 0.7;
-  }
-`;
-
-const NavIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  background-color: #333;
-  margin-bottom: 4px;
-  border-radius: 2px;
-
-  /* 아이콘을 간단한 도형으로 표현 */
-  &:nth-of-type(1) {
-    /* 홈 아이콘 - 집 모양 */
-    clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
-  }
-`;
-
-const HomeIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  background-color: #333;
-  margin-bottom: 4px;
-  border-radius: 2px;
-  position: relative;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 4px;
-    left: 4px;
-    right: 4px;
-    bottom: 8px;
-    border: 2px solid #333;
-    border-bottom: none;
-    border-radius: 2px 2px 0 0;
-  }
-
-  &::after {
-    content: '';
-    position: absolute;
-    top: 8px;
-    left: 8px;
-    right: 8px;
-    bottom: 4px;
-    background-color: #333;
-  }
-`;
-
-const SearchIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  background-color: #333;
-  margin-bottom: 4px;
-  border-radius: 50%;
-  position: relative;
-
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: -2px;
-    right: -2px;
-    width: 8px;
-    height: 2px;
-    background-color: #333;
-    transform: rotate(45deg);
-  }
-`;
-
-const DownloadIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  background-color: #333;
-  margin-bottom: 4px;
-  border-radius: 2px;
-  position: relative;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 4px;
-    left: 6px;
-    right: 6px;
-    height: 2px;
-    background-color: #f5f5dc;
-  }
-
-  &::after {
-    content: '';
-    position: absolute;
-    top: 2px;
-    left: 10px;
-    width: 4px;
-    height: 4px;
-    background-color: #f5f5dc;
-    border-radius: 50%;
-  }
-`;
-
-const ProfileIcon = styled.div`
-  width: 24px;
-  height: 24px;
-  background-color: #333;
-  margin-bottom: 4px;
-  border-radius: 50%;
-  position: relative;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 4px;
-    left: 6px;
-    right: 6px;
-    height: 8px;
-    background-color: #f5f5dc;
-    border-radius: 4px 4px 0 0;
-  }
-
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: 2px;
-    left: 8px;
-    right: 8px;
-    height: 6px;
-    background-color: #f5f5dc;
-    border-radius: 0 0 3px 3px;
-  }
-`;
