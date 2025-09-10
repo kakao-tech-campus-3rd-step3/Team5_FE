9a8a87278ae1b4c7a45c326df11739ca48b0f42f
Changmok LEE <cmlee5075@gmail.com>
2025-09-10 17:43:45 +0900

    feat: routes 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
-// import FeedbackPage from './pages/Feedback';
-import ArchivePage from './pages/Archive/Archive';
-import FeedbackPage from './pages/Feedback/Feedback';
-// import SubscribePage from './pages/subscribe';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/* <ArchivePage /> */}
-      {/* <SubscribePage /> */}
-      <FeedbackPage />
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
