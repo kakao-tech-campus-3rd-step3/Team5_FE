8c561cf65d292f132532cceaa382ef25ed0b7bd9
JinVibe <loonaticvibe2.11@gmail.com>
2025-09-10 10:06:23 +0900

    fix: 디렉토리 구조 수정에 따라 로직 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { theme } from './styles/theme';
 // import FeedbackPage from './pages/Feedback';
 import ArchivePage from './pages/Archive/Archive';
 import FeedbackPage from './pages/Feedback/Feedback';
+import SubscribePage from './pages/Subscribe/Subscribe';
 // import SubscribePage from './pages/subscribe';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* <ArchivePage /> */}
-      {/* <SubscribePage /> */}
-      <FeedbackPage />
+      <SubscribePage />
+      {/* <FeedbackPage /> */}
     </ThemeProvider>
   );
 }
