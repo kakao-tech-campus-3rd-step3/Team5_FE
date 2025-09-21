03c653bb08a319d09d7694c1cb4ce884fe60482b
Changmok LEE <cmlee5075@gmail.com>
2025-09-21 22:22:59 +0900

    refactor: Lottie 위치 ScrollButton과 동기화

--- src/pages/Archive/Archive.tsx
+++ src/pages/Archive/Archive.tsx
@@ -3,8 +3,8 @@ import Description from './components/Description';
 import PinnedQuestionList from './components/PinnedQuestionList';
 import QuestionList from './components/QuestionList';
 import useSectionScroll from './hooks/useSectionScroll';
-// import Lottie from 'lottie-react';
-// import clickkAnimation from './assets/lottie/clickIcon.json';
+import Lottie from 'lottie-react';
+import clickAnimation from './assets/lottie/clickIcon.json';
 
 const ArchivePage = () => {
   const {
@@ -16,22 +16,26 @@ const ArchivePage = () => {
       <SectionFirst ref={sectionFirstRef}>
         <Description />
         <ButtonWrapper>
-          <ScrollDownButton type="button" onClick={handleDownClick}>
+          <ScrollButton type="button" onClick={handleDownClick}>
             down
-          </ScrollDownButton>
+          </ScrollButton>
+          <LottieWrapper>
+            <Lottie animationData={clickAnimation} loop autoplay />
+          </LottieWrapper>
         </ButtonWrapper>
-        {/* <LottieWrapper> */}
-        {/* <Lottie animationData={clickkAnimation} loop autoplay /> */}
-        {/* </LottieWrapper> */}
-
         <PinnedQuestionList />
       </SectionFirst>
 
       <SectionSecond ref={sectionSecondRef}>
         <QuestionList />
-        <ScrollUpButton type="button" onClick={handleUpClick}>
-          Up
-        </ScrollUpButton>
+        <ButtonWrapper>
+          <ScrollButton type="button" onClick={handleUpClick}>
+            Up
+          </ScrollButton>
+          <LottieWrapper>
+            <Lottie animationData={clickAnimation} loop autoplay />
+          </LottieWrapper>
+        </ButtonWrapper>
       </SectionSecond>
     </Wrapper>
   );
@@ -70,29 +74,24 @@ const SectionSecond = styled.section`
 
 const ButtonWrapper = styled.div`
   position: absolute;
+  bottom: 120px;
   left: 50%;
-  bottom: 10%;
   transform: translate(-50%, -50%);
 `;
 
-const ScrollDownButton = styled.button`
-  width: 100%;
-  height: 100%;
-`;
-
-const ScrollUpButton = styled.button`
+const ScrollButton = styled.button`
   position: absolute;
+  top: 50%;
   left: 50%;
-  bottom: 10%;
   transform: translate(-50%, -50%);
 `;
 
-// const LottieWrapper = styled.div`
-//   position: absolute;
-//   left: 50%;
-//   bottom: 10%;
-//   transform: translate(-50%, -50%);
-
-//   // width: 80px;
-//   // height: 80px;
-// `;
+const LottieWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 120px;
+  height: 120px;
+  pointer-events: none;
+`;
