c4a61685844e4abf26bd2f38ad29cfae1ea4c9c8
Changmok LEE <cmlee5075@gmail.com>
2025-09-23 22:21:15 +0900

    feat: ROUTE_PATH 상수 경로 설정

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { ROUTE_PATH } from './routePath';
 
 interface ProtectedRouteProps {
   isAuth: boolean;
@@ -7,7 +8,7 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ isAuth }: ProtectedRouteProps) => {
   if (!isAuth) {
     // TODO: 로그인이 안되어있으면 로그인 페이지로 리다이렉트 혹은 익스텐션 설치 페이지로 이동
-    return <Navigate to="/login" replace />;
+    return <Navigate to={ROUTE_PATH.LOGIN} replace />;
   }
   return <Outlet />;
 };
